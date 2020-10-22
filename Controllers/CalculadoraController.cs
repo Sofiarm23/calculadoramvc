7ae70fb154438bdf93cf6250831daa4c8541e5ad
caroline ruiz <caroline_ruiz@usmp.pe>
2020-10-22 01:06:10 -0500

    first commit

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using calculadoramvc.Models;
+
+namespace calculadoramvc.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        private readonly ILogger<CalculadoraController> _logger;
+
+        public CalculadoraController(ILogger<CalculadoraController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace calculadoramvc.Models
+{
+    public class Calculadora
+    {
+        public int Operador1 { get; set; }
+
+        public int Operador2 { get; set; }
+
+        public String Accion { get; set; }
+
+        public int Respuesta { get; set; }
+
+        
+    }
+}
